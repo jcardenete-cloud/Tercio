2ee62d62e98eb33a93fcb9f7aa14a656d5032366
Fco. Javier <jcardenete@gmail.com>
2026-07-20 09:48:51 +0200

    Eliminación de ficheros superfluos

--- stop_app.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Deteniendo servicios de Control Horario...
-
-:: Detener procesos de Node/npm
-taskkill /IM node.exe /F 2>nul
-
-echo Aplicacion detenida correctamente.
-timeout /t 3
